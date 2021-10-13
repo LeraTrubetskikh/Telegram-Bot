479c71b6486bd689d71f2ea2b5007ca1d0d5e71d
LeraTrubetskikh <trubetzkih.lera@gmail.com>
2021-10-13 21:27:44 +0500

    Подключение к Telegram

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,61 +1,71 @@
 import Questions.Question;
 import Questions.QuestionGenerator;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class Bot {
+public class Bot extends TelegramLongPollingBot {
 
-    private Scan scanner;
-    private boolean runTheProgramFlag;
     private QuestionGenerator questionGenerator;
+    private String token;
+    private String username;
+    private boolean firstQuestionFlag = true;
+    private Question question;
 
-    public Bot() {
-        scanner = new Scan();
-        runTheProgramFlag = true;
+    public Bot(String token, String username) {
+        this.token = token;
+        this.username = username;
         questionGenerator = new QuestionGenerator();
     }
 
-    public void start() {
-        sendMessage("Введите /help");
-        while(runTheProgramFlag) {
-            checkCommand(getMessage());
-        }
-    }
-
-    public void startNewGame() {
-        String message;
-        Question question = new Question("", "");
-
-        while(runTheProgramFlag) {
+    public String getNewMessage(String message) {
+        if (firstQuestionFlag) {
             question = questionGenerator.getQuestion();
-            sendMessage(question.question);
-            message = getMessage();
-            if(!checkCommand(message)) {
-                if (message.equals(question.answer))
-                    sendMessage("Правильно!");
-                else
-                    sendMessage("Не правильно!");
+            firstQuestionFlag = false;
+            return question.question;
+        }
+        else {
+            if (message.equals(question.answer)) {
+                question = questionGenerator.getQuestion();
+                return "Правильно!" + ":" + question.question;
+            }
+            else {
+                question = questionGenerator.getQuestion();
+                return "Неправильно!" + ":" + question.question;
             }
         }
     }
 
-    public boolean checkCommand(String str){
-        if ("/help".equals(str)) {
-            scanner.printHelp();
-            return true;
-        } else if ("/newgame".equals(str)) {
-            startNewGame();
-            return true;
-        } else if ("/stop".equals(str)) {
-            runTheProgramFlag = false;
-            return true;
+    @Override
+    public void onUpdateReceived(Update update) {
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+                Message inMessage = update.getMessage();
+
+                String s = getNewMessage(inMessage.getText());
+                String[] list = s.split(":"); // строка - Правильно/Неправильно:Следующий вопрос
+                for (String value : list) {
+                    SendMessage outMessage = new SendMessage();
+                    outMessage.setChatId(inMessage.getChatId().toString());
+                    outMessage.setText(value);
+                    execute(outMessage);
+                }
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
-        return false;
     }
 
-    public String getMessage() {
-        return scanner.getLine();
+    @Override
+    public String getBotToken() {
+        return token;
     }
 
-    public void sendMessage(String str) {
-        scanner.printLine(str);
+    @Override
+    public String getBotUsername() {
+        return username;
     }
+
 }
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,11 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class Program {
-    public static void main(String[] args) {
-        Bot bot = new Bot();
-        bot.start();
+    public static void main(String[] args) throws TelegramApiRequestException {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        botsApi.registerBot(new Bot("2090752243:AAF__7tTqJFtwc59sukdjAw0G6377l431Ko", "@matmex_geographer_bot"));
     }
 }
