beaff48066f0291790fd58ccbba20bb5fcab9ab0
LeraTrubetskikh <trubetzkih.lera@gmail.com>
2021-11-29 18:32:07 +0500

    Реализованы разделение по регионам и статистика по регионам

--- src/main/java/BotLogic/BotLogic.java
+++ src/main/java/BotLogic/BotLogic.java
@@ -4,46 +4,50 @@ import Questions.Question;
 import Questions.QuestionGenerator;
 import Users.User;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class BotLogic {
 
     private BotMessage message;
     private final HashMap<Long, User> users;
+    private Long userId;
     private final QuestionGenerator questionGenerator;
     private Question question;
+    private final String[] regions;
 
     public BotLogic(){
         message = new BotMessage("", 0L);
         users = new HashMap<>();
         questionGenerator = new QuestionGenerator();
         question = new Question();
+        regions = new String[]{"Европа", "Азия", "Америка", "Африка", "Австралия и Океания"};
     }
 
     public void readUpdate(BotMessage message) {
-        this.message = message;
+        this.message = message; // а этот метод вообще нужен? типа можно же просто сразу передовать это сообщение в getanswer
     }
 
     public BotMessage getAnswer(){
         String text = message.getText();
-        Long id = message.getUserId();
+        userId = message.getUserId();
 
-        if (!users.containsKey(id)) {
-            users.put(id, new User(id));
+        if (!users.containsKey(userId)) {
+            users.put(userId, new User(userId));
         } else {
-            question = users.get(id).lastQuestion;
+            question = users.get(userId).lastQuestion;
         }
 
         if (!text.isEmpty() && text.charAt(0) == '/') {
             text = responseToCommand(text);
         }
-        else if(users.get(id).gameMode){
+        else if(users.get(userId).gameMode){
             text = responseToMessage(text);
         }
         else {
             text = "Для начала введите /start";
         }
-        users.get(id).setQuestion(question);
+        users.get(userId).setQuestion(question);
         BotMessage newMessage = new BotMessage("", 0L);
         newMessage.setUserId(message.getUserId());
         newMessage.setText(text);
@@ -51,38 +55,43 @@ public class BotLogic {
     }
 
     private String responseToCommand(String text) {
-        var outputMsg = "";
         switch (text) {
             case ("/start"):
-                outputMsg = "Привет! Я бот для изучения столиц стран мира.\n" +
+                return "Привет! Я бот для изучения столиц стран мира.\n" +
                         "Чтобы посмотреть список моих комманд, введите /help";
-                break;
             case ("/help"):
-                outputMsg = "/start - информация о боте,\n" +
+                return "/start - информация о боте,\n" +
                         "/newgame - начать новую игру,\n" +
-                        "/stop - остановить игру";
-                break;
+                        "/stop - остановить игру\n" +
+                        "/stat - статистика по всем регионам";
             case ("/stop"):
-                users.get(message.getUserId()).gameMode = false;
-                break;
+                users.get(userId).gameMode = false;
+                return String.format("Ваш счёт: %d", users.get(userId).getScore());
             case ("/newgame"):
-                users.get(message.getUserId()).gameMode = true;
-                question = questionGenerator.getQuestion("Европа");
-                outputMsg = question.getQuestion();
-                break;
+                users.get(userId).gameMode = true;
+                return "Выберите регион: \nЕвропа,\nАзия,\nАмерика,\nАфрика,\nАвстралия и Океания";
+                // тут все вылетает когда неправильно вводишь название региона
+            case ("/stat"):
+                return users.get(userId).getStat();
             default:
-                outputMsg = "Это не команда";
-                break;
+                return "Это не команда";
         }
-        return outputMsg;
     }
 
     private String responseToMessage(String msg){
+        if (Arrays.asList(regions).contains(msg)){
+            users.get(userId).resetScore();
+            users.get(userId).region = msg;
+            question = questionGenerator.getQuestion(msg);
+            return question.getQuestion();
+        }
         if (msg.equals(question.getAnswer())) {
-            question = questionGenerator.getQuestion("Европа");
+            question = questionGenerator.getQuestion(users.get(userId).region);
+            users.get(userId).addPoints();
+            users.get(userId).updateScore();
             return "Правильно!" + "\n" + question.getQuestion();
         } else {
-            question = questionGenerator.getQuestion("Европа");
+            question = questionGenerator.getQuestion(users.get(userId).region);
             return "Неправильно!" + "\n" + question.getQuestion();
         }
     }
--- src/main/java/Users/User.java
+++ src/main/java/Users/User.java
@@ -2,25 +2,62 @@ package Users;
 
 import Questions.Question;
 
+import java.util.HashMap;
+
 public class User {
 
     private final Long id;
-    private int points;
+    private int score;
     public Question lastQuestion;
     public Boolean gameMode;
     public String region;
+    public HashMap<String, Integer> bestScore;
 
     public User(Long id) {
         this.id = id;
-        points = 0;
+        score = 0;
         lastQuestion = new Question();
         gameMode = false;
         region = "";
+        bestScore = new HashMap<>();
+        bestScore.put("Европа", 0);
+        bestScore.put("Азия", 0);
+        bestScore.put("Америка", 0);
+        bestScore.put("Африка", 0);
+        bestScore.put("Австралия и Океания", 0);
     }
 
-    public Long getID() { return id; }
-    public int getPoints() { return points; }
-    public void setQuestion(Question question) { lastQuestion = question; }
-    public void addPoints() { points++; }
+    public Long getID() { // ничгде не используется но пусть будет?
+        return id;
+    }
 
+    public int getScore() {
+        return score;
+    }
+
+    public void setQuestion(Question question) {
+        lastQuestion = question;
+    }
+
+    public void addPoints() { // названия?
+        score++;
+    }
+
+    public void resetScore() {
+        score = 0;
+    }
+
+    public void updateScore() {
+        if (bestScore.get(region) < score)
+            bestScore.put(region, score);
+    }
+
+    public String getStat() {
+        StringBuilder sb = new StringBuilder();
+
+        for (String key : bestScore.keySet()) {
+            sb.append(String.format("%s: %d\n", key, bestScore.get(key)));
+        }
+        return sb.toString();
+    }
 }
